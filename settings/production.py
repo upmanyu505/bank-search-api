2cea24dff61f01ff4f8c8747fefd9ce476315d1d
Upmanyu Sharma <upmanyu505@gmail.com>
2020-02-14 13:40:57 +0530

    updates

--- settings/production.py
+++ settings/production.py
@@ -9,6 +9,7 @@ ALLOWED_HOSTS = ['bank-search-api.herokuapp.com']
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES = {}
+DATABASES['default'] = {}
 DATABASES['default'].update(prod_db)
 
 django_heroku.settings(locals())
